59b64d4f1997069a2df550a862f9538cbfeddef7
rajansingh387 <119110954+rajansingh387@users.noreply.github.com>
2023-06-21 22:06:03 +0530

    Add files via upload

---
+++ course_details.py
@@ -0,0 +1,9 @@
+import os, sys
+from os.path import dirname,join,abspath
+sys.path.insert(0, abspath(join(dirname(__file__),'..')))
+#for no moudule found
+
+
+def course():
+    print('this is my course details')
+
